81ba01f9ab40157434b788019ab7ae22174522de
Pidddd <424f1d@gmail.com>
2026-09-25 10:22:25 +0700

    cek menu item

--- src/main/java/id/ac/polinema/oop/MenuItem.java
+++ src/main/java/id/ac/polinema/oop/MenuItem.java
@@ -19,16 +19,20 @@ public class MenuItem {
      * @param name  item name
      * @param price price in Rupiah
      */
+
+    private String name;
+    private Double price;
+
     public MenuItem(String name, double price) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        this.name = name;
     }
 
     public String getName() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return this.name;
     }
 
     public double getPrice() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return this.price;
     }
 
     /**
@@ -38,6 +42,8 @@ public class MenuItem {
      * @param price new price in Rupiah
      */
     public void setPrice(double price) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        if (price >= 0) {
+            this.price = price;
+        }
     }
 }
