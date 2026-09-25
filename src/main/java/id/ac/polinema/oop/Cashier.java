62e6769880296f5cef349a1c81a3977f4ba0b06a
Pidddd <424f1d@gmail.com>
2026-09-25 11:09:33 +0700

    test Cashier

---
+++ src/main/java/id/ac/polinema/oop/Cashier.java
@@ -0,0 +1,12 @@
+package id.ac.polinema.oop;
+
+public class Cashier {
+    public double calculateChange(Order order, double cash) {
+        double finalTotal = order.getFinalTotal();
+        if (cash >= finalTotal) {
+            return cash - finalTotal;
+        } else{
+            return 0;
+        }
+    }
+}
